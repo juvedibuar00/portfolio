8f698d45953a25a7f435d45f9418acaf530d4265
juvedibuar00 <130672236+juvedibuar00@users.noreply.github.com>
2024-11-16 13:03:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Exemplo: Alerta quando o formulário for enviado (você pode expandir isso com validação, etc.)
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previne o envio do formulário
+    alert("Mensagem enviada com sucesso!");
+  });
+  
